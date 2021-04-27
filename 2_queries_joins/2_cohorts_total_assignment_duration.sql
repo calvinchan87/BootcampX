009ed20f831b8c7fdf7dc54f081affb3ca8dfcbb
Calvin <calvinchan87@gmail.com>
2021-04-27 03:20:13 +0000

    second round of queries

---
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -0,0 +1,7 @@
+SELECT SUM(duration) as total_duration
+FROM students
+JOIN cohorts
+ON cohorts.id = students.cohort_id
+JOIN assignment_submissions
+ON assignment_submissions.student_id = students.id
+WHERE cohorts.name = 'FEB12';
